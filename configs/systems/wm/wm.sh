c6fb9dd0f32bff8989d1f5b0e2cd069aa1c35260
leg7 <dev@leonardgomez.xyz>
2025-09-16 20:46:39 +0200

    Pipewire pulse enable and start it with river

--- configs/systems/wm/wm.sh
+++ configs/systems/wm/wm.sh
@@ -34,6 +34,7 @@ cd -
 sudo xbps-install -y pipewire
 mkdir -p /etc/pipewire/pipewire.conf.d
 ln -sf /usr/share/examples/wireplumber/10-wireplumber.conf /etc/pipewire/pipewire.conf.d/
+ln -sf /usr/share/examples/pipewire/20-pipewire-pulse.conf /etc/pipewire/pipewire.conf.d/
 
 # Transmission torrents, vial, logseq, rivercarro, door-knocker, banana cursor, mime apps, portals, xdg-dirs, pipewire
 # autologin
