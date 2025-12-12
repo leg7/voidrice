e375b541faa425e1b498bab939a1383999405dd6
leg7 <leonard.gomez@protonmail.com>
2025-12-12 18:27:35 +0100

    hardware: Add config for "dan" desktop

---
+++ hardware-configs/dan/dan.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+sudo xbps-install -y void-repo-nonfree amdvlk amdvlk-32bit linux-firmware-amd
+
+sudo xbps-install -y mesa-dri vulkan-loader
+
+sudo xbps-install -y iwd dbus
+sudo ln -sf /etc/sv/dbus/ /var/service/
+sudo ln -sf /etc/sv/iwd/ /var/service/
+
+(
+	cd ../../software-configs/wm || exit
+	./wm.sh
+)
